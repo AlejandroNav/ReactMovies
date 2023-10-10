21bf0207bb0e91ee1aadcf354391cf271abf8512
Alex-Nav <alejugger@gmail.com>
2023-10-10 01:03:46 -0600

    finished dice game

--- movie1/src/Dice.js
+++ movie1/src/Dice.js
@@ -1,19 +1,32 @@
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDiceOne, faDiceTwo, faDiceThree, faDiceFour, faDiceFive, faDiceSix } from '@fortawesome/free-solid-svg-icons';
+import './Dice.css';
 const diceIcons = [faDiceOne, faDiceTwo, faDiceThree, faDiceFour, faDiceFive, faDiceSix];
 
 function Dice() {
-    const [diceNumber, setDiceNumber] = useState(0);
+    const [die, setDiceNumber] = useState(0);
+    const [die2, setDiceNumber2] = useState(0);
+    const [isRolling, setIsRolling] = useState(false);
+
     function rollDice() {
-        const randomNumber = Math.floor(Math.random() * 6);
-        setDiceNumber(randomNumber);
+        setIsRolling(true);
+        setTimeout(() => {
+            const randomNumber1 = Math.floor(Math.random() * 6);
+            const randomNumber2 = Math.floor(Math.random() * 6);
+            setDiceNumber(randomNumber1);
+            setDiceNumber2(randomNumber2);
+            setIsRolling(false);
+        },1500); // Tiempo de espera de 2 segundos.
     }
 
     return (
-        <div>
-            <FontAwesomeIcon icon={diceIcons[diceNumber]} className="customDiceIcon" /> <br />
-            <button onClick={rollDice}>Roll Dice</button>
+        <div className='RollDice'>
+            <FontAwesomeIcon icon={diceIcons[die]} className={`customDiceIcon ${isRolling ? 'rolling' : ''}`} />
+            <FontAwesomeIcon icon={diceIcons[die2]} className={`customDiceIcon ${isRolling ? 'rolling' : ''}`} />
+            <button onClick={rollDice} disabled={isRolling}>
+                {isRolling ? "Rolling..." : "Roll Dice"}
+            </button>
         </div>
     );
 }
